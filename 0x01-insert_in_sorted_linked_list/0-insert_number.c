898dfe21f95e19f1e4316e47ca8e4727856b9241
ledbagholberton <abdel.perez@gmail.com>
2020-02-11 23:20:16 -0500

    fix erros betty

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
 /**
  * print_listint - prints all elements of a listint_t list
  * @h: pointer to head of list
@@ -9,38 +8,34 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new;
-    listint_t *current;
-    listint_t *past;
-
-    current = *head;
-    past = *head;
-
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->n = number;
-    new->next = NULL;
-
-    if (*head == NULL)
-        *head = new;
-    else if (current->next == NULL && current->n > number)
-    {
-        new->next = current;
-        *head = new;
-    }
-    else
-    {
-        while (current->n < number || current->next != NULL)
-        {
-            past = current;
-            current = current->next;
-            printf("En el loop %i\n", current->n);
-        }
-        past->next = new;
-        if (current != NULL)
-            new->next = current;
-    }
-    return (new);
+listint_t *new;
+listint_t *current;
+listint_t *past;
+current = *head;
+past = *head;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+    return (NULL);
+new->n = number;
+new->next = NULL;
+if (*head == NULL)
+    *head = new;
+else if (current->next == NULL && current->n > number)
+  {
+     new->next = current;
+     *head = new;
+  }
+else
+  {
+     while (current->n < number || current->next != NULL)
+       {
+	 past = current;
+	 current = current->next;
+	 printf("En el loop %i\n", current->n);
+       }
+    past->next = new;
+    if (current != NULL)
+      new->next = current;
+  }
+return (new);
 }
