34bf6b25527c4a3ff1335886d0f3f8cfa605e754
ledbagholberton <abdel.perez@gmail.com>
2020-02-11 23:45:55 -0500

    fix

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -2,40 +2,42 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * insert_node - insert a node
+ * @head: pointer to head of list
+ * @number: number to insert
+ * Return: new node
  */
 listint_t *insert_node(listint_t **head, int number)
 {
 listint_t *new;
 listint_t *current;
 listint_t *past;
+
 current = *head;
 past = *head;
 new = malloc(sizeof(listint_t));
 if (new == NULL)
-    return (NULL);
+   return (NULL);
 new->n = number;
 new->next = NULL;
 if (*head == NULL)
-    *head = new;
+  *head = new;
 else if (current->next == NULL && current->n > number)
   {
-     new->next = current;
-     *head = new;
+    new->next = current;
+    *head = new;
   }
 else
   {
-     while (current->n < number || current->next != NULL)
-       {
-	 past = current;
-	 current = current->next;
-	 printf("En el loop %i\n", current->n);
-       }
+    while (current->n < number || current->next != NULL)
+      {
+	past = current;
+	current = current->next;
+	printf("En el loop %i\n", current->n);
+      }
     past->next = new;
     if (current != NULL)
       new->next = current;
   }
-return (new);
+ return (new);
 }
