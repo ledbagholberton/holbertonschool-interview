18cda24b029d6fdccbfefe7b124c193adc9c1746
ledbagholberton <abdel.perez@gmail.com>
2020-02-18 16:00:35 -0500

    Create a Node

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,21 +1,20 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_node - function that creates a binary tree node
- * @value: Integer with the value in node created
- * @parent: is a pointer to the parent node of the node to create
- * Return: pointer to the new node, or NULL on failure
- */
+ * binary_tree_node - creates a node in the binary tree
+ * @parent: pointer to a parent node
+ * @value: number value in the node
+ * Return: pointer to the new node
+**/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *new;
+	binary_tree_t *node;
 
-new = malloc(sizeof(binary_tree_t));
-if (new == NULL)
-	return (NULL);
-new->n = value;
-new->parent = parent;
-new->left = NULL;
-new->right = NULL;
-return (new);
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -3,8 +3,64 @@
 #include "binary_trees.h"
 
 /**
- * main - Entry point
+ * heap_insert - function that inserts a value into a Max Binary Heap
  *
- * Return: 0 on success, error code on failure
+ * @root: double pointer to the root node of the Heap
+ * @value: value in the node to be inserted
+ *
+ * Return:  a pointer to the inserted node, or NULL on failure
  */
 
+heap_t *heap_insert(heap_t **root, int value)
+{
+    size_t height;
+    
+    height = _height(*root);
+    for (i = 0, height, i = i+1)
+        {
+            search
+        }
+
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the node
+ * Return: height of the tree from #include "binary_trees.h"node
+ **/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	right = binary_tree_height(tree->right) + 1;
+	left = binary_tree_height(tree->left) + 1;
+	if (left > right)
+		return (left);
+	else
+		return (right);
+}
+
+/**
+ * _search - Found if there is a empty node
+ *
+ * @tree: Pointer to the node to search 
+ *
+ * Return: The pointer to parent node with empty
+ */
+heap_t *search(binary_tree_t *tree)
+{
+	size_t height_l;
+	size_t height_r;
+
+    if (tree->right == NULL || tree->right == NULL)
+        return(tree)
+    
+
+	height_l = tree->left ? 1 + _height(tree->left) : 0;
+	height_r = tree->right ? 1 + _height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
