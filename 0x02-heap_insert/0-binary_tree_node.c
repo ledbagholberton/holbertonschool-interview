b5f6b26c6143252cee87285dea201d83bc4c90f6
ledbagholberton <abdel.perez@gmail.com>
2020-02-18 10:01:55 -0500

    Bettyś fiix

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -5,18 +5,17 @@
 /**
  * binary_tree_node - function that creates a binary tree node
  * @value: Integer with the value in node created
- * @parent: is a pointer to the parent node of the node to create   
+ * @parent: is a pointer to the parent node of the node to create
  * Return: pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+binary_tree_t *new;
 
-    new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
-		return (NULL);
-    
-    new->n = value;
-    new->parent = parent;
-    return(new);
+new = malloc(sizeof(binary_tree_t));
+if (new == NULL)
+	return (NULL);
+new->n = value;
+new->parent = parent;
+return (new);
 }
