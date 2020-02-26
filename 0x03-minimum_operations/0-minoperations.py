ceb5a6f29c1ecefcb0ac6b4087b26c6833f7c697
ledbagholberton <789@holbertonschool.com>
2020-02-26 04:20:54 +0000

    Changing the alg

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,17 +9,12 @@ def minOperations(n):
     cost = 0
     if type(n) is not int:
         return 0
-    if n <= 0 or type(n) is not int:
+    if n < 2:
         return 0
-    while (n > 0):
-        if (n % 2) != 0:
-            cost = cost + 1
-            n = n - 1
-        else:
-            temp = n / 2
-            if (temp > 2):
-                cost = cost + 2
-            else:
-                cost = cost + temp
-            n = n / 2
+    i = 2
+    while (i<=n):
+        while (n % i == 0):
+            cost += i
+            n = n/i
+        i += 1
     return int(cost)
