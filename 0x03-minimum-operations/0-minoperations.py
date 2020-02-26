9c239574ab494c2174fb9a7d60de22378547e911
ledbagholberton <789@holbertonschool.com>
2020-02-26 03:44:35 +0000

    Minimal operations + pep8

---
+++ 0x03-minimum-operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+
+def minOperations(n):
+    """Function minOperations"""
+    cost = 0
+    if n <= 0 or type(n) is not int:
+        return 0
+    while (n > 0):
+        if (n % 2) != 0:
+            cost = cost + 1
+            n = n - 1
+        else:
+            temp = n / 2
+            if (temp > 2):
+                cost = cost + 2
+            else:
+                cost = cost + temp
+            n = n / 2
+    return cost
