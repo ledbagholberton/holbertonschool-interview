6dfca11349a7f2abe8d97b652eb33b5abcf79a8f
ledbagholberton <789@holbertonschool.com>
2020-03-09 23:27:38 +0000

    Fxin issue in .H

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,9 +2,6 @@
 #ifndef sandpiles_h_
 #define sandpiles_h_
 #include <stdlib.h>
-=======
-#ifndef SAND_H
-#define SAND_H
 
 void print_grid_sum(int grid1[3][3], int grid2[3][3]);
 void print_grid(int grid[3][3]);
