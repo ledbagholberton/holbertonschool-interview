2c0f277cae825f678532458164d6d2325feb8586
ledbagholberton <789@holbertonschool.com>
2020-03-19 03:13:21 +0000

    Include : according to format

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,18 +11,19 @@ try:
     for data in sys.stdin:
         parsed_data = re.split(r'[-+#\s*$ ]', data)
         size_parsed = len(parsed_data)
-        count += 1
-        size_file = int(parsed_data[size_parsed - 2])
-        total_size += size_file
-        error = parsed_data[size_parsed - 3]
-        if error in dict_error:
-            dict_error[error] += 1
-        else:
-            dict_error[error] = 1
+        if size_parsed > 2:
+            count += 1
+            size_file = int(parsed_data[size_parsed - 2])
+            total_size += size_file
+            error = parsed_data[size_parsed - 3]
+            if error in dict_error:
+                dict_error[error] += 1
+            else:
+                dict_error[error] = 1
         if count == 10:
             print("File size: {}".format(total_size))
             for k, v in sorted(dict_error.items()):
-                print(k, v)
+                print("{}: {}".format(k, v))
             count = 0
 except KeyboardInterrupt:
     print("File size: {}".format(total_size))
