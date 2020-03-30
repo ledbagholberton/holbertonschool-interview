06d2fe6bc9b4a82d6fec52e8717a0f4ca7a5e166
ledbagholberton <789@holbertonschool.com>
2020-03-30 20:05:21 +0000

    Betty

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * is_palindrome - FUnction to determine 
+ * is_palindrome - FUnction to determine
  *
  * @n: Number
  *
@@ -10,48 +10,54 @@
  */
 int is_palindrome(unsigned long n)
 {
-    int fs, ls, sz;
-    unsigned long pow, n1, n2;
+int fs, ls, sz;
+unsigned long pow, n1, n2;
 
-    fs = first(n);
-    ls = last(n);
-    sz = len(n);
-    printf("%i is first and %i is last\n", fs, ls);
-    if (sz > 2)
-    {
-        if (fs != ls)
-            return(0);
-        else
+fs = first(n);
+ls = last(n);
+sz = len(n);
+if (sz > 2)
+{
+    if (fs != ls)
+        return (0);
+    else
         {
             pow = power(10, sz - 1);
-            n1 = n - (fs * pow);
+            n1 = n - (fs *pow);
             n2 = (n1 - ls) / 10;
-            printf("nuevo n es %lu\n", n2);
-            is_palindrome(n2);
+            return (is_palindrome(n2));
         }
-    }
-    else if(sz == 2)
-    {
-        printf("Len es menor que 3. Len = %i\n", sz);
-        if (fs != ls)
-            return(0);
-        else
-            return(1);
-    }
-    return(1);
+}
+else if (sz == 2)
+    if (fs != ls)
+        return (0);
+    return (1);
 }
 
+/**
+ * power - FUnction power
+ *
+ * @base: Number
+ * @exponent: Number
+ * Return: base ^ exponent
+ */
 unsigned long power(int base, int exponent)
 {
-    int result=1, a;
+    int a;
+    unsigned long result = 1;
 
     for (a = exponent; a > 0; a = a - 1)
-    {
         result = result * base;
-    }
-    return result;
+    return (result);
 }
 
+/**
+ * len - size of number
+ *
+ * @n: Number
+ *
+ * Return: size of number
+ */
 int len(unsigned long n)
 {
     int count = 0;
@@ -64,6 +70,13 @@ int len(unsigned long n)
     return (count);
 }
 
+/**
+ * last - last digit in n
+ *
+ * @n: Number
+ *
+ * Return: last digit
+ */
 unsigned long last(unsigned long n)
 {
     unsigned long last;
@@ -72,12 +85,19 @@ unsigned long last(unsigned long n)
     return (last);
 }
 
+/**
+ * first - first digit in n
+ *
+ * @n: Number
+ *
+ * Return: first digit
+ */
 unsigned long first(unsigned long n)
 {
     unsigned long first;
 
     first = n;
-    while(first >= 10)
+    while (first >= 10)
         first = first / 10;
     return (first);
 }
