9ae22b1a040f4f2eee0cff3f516971c91f80ed19
ledbagholberton <789@holbertonschool.com>
2020-05-27 18:08:03 +0000

    Skip line

---
+++ 0x0E-linear_skip/skiplist/search_algos.h
@@ -0,0 +1,29 @@
+#ifndef searchalgos_h_
+#define searchalgos_h_
+#include <stdlib.h>
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+void init_express(skiplist_t *list, size_t size);
+skiplist_t *create_skiplist(int *array, size_t size);
+void free_skiplist(skiplist_t *list);
+void print_skiplist(const skiplist_t *list);
+
+#endif
