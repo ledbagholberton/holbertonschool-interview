c6b728c8bc0c491df70dfec01b178b23ba43ca9c
ledbagholberton <789@holbertonschool.com>
2020-05-27 18:29:10 +0000

    Skip line - betty

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -7,36 +7,40 @@
  *
  * @list: Pointer to the linked list to be freed
  * @value: Value to search in linkedlist
+ *
+ * Return: Value found in index
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *node;
 
 	if (!list)
-	  return NULL;
-
+		return (NULL);
 	node = list->express;
-	printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%lu] = [%d]\n",
+			node->index, node->n);
 
 	while (node != NULL && node->n < value)
-	  {
-	    list = node;
-	    node = node->express;
-	    if (!node)
-	      {
-		node = list;
-		while (node->next)
-		  node = node->next;
-		break;
-	      }
-	    printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
-	  }
-	printf("Value found between indexes [%lu] and [%lu]\n", list->index, node->index);
-	for (;list != NULL && node->n >= list->n; list = list->next)
+		{
+			list = node;
+			node = node->express;
+			if (!node)
+			{
+				node = list;
+				while (node->next)
+					node = node->next;
+				break;
+			}
+			printf("Value checked at index [%lu] = [%d]\n",
+					node->index, node->n);
+		}
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			list->index, node->index);
+	for (; list != NULL && node->n >= list->n; list = list->next)
 	{
 		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
 		if (list->n == value)
-			return(list);
+			return (list);
 	}
-	return NULL;
+	return (NULL);
 }
