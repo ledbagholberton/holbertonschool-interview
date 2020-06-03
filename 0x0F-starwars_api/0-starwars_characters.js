e459645c5048e66adf6eb5e357743fc3347536c0
ledbagholberton <789@holbertonschool.com>
2020-06-03 21:09:08 +0000

    Test

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,25 +1,45 @@
 #!/usr/bin/node
+/*
+    Write a script that prints all characters of a Star Wars movie:
+    The first positional argument passed is the Movie ID -
+    example: 3 = “Return of the Jedi”
+    Display one character name per line in the same order
+    as the “characters” list in the /films/ endpoint
+    You must use the Star wars API
+    You must use the request module
+*/
 
-const request = require('request');
 const args = process.argv.slice(2);
-const url = 'http://swapi-api.hbtn.io/api/films/' + args[0];
+if (args < 1) {
+    console.log('Error');
+    process.exit(1);
+}
 
-request(url, async function (err, res, body) {
-  if (err || res.statusCode !== 200) {
-    console.log(err);
-  } else {
-    const myList = JSON.parse(body).characters;
-    for (const urlChar of myList) {
-      const respChar = await new Promise((resolve, reject) => {
-        request(urlChar, (error, response, html) => {
-          if (error || res.statusCode !== 200) {
-            reject(error);
-          } else {
-            resolve(JSON.parse(html).name);
-          }
-        });
-      });
-      console.log(respChar);
+const episode = args[0];
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + episode;
+const request = require('request');
+let characters = [];
+request(url, (err, resp, body) => {
+    if (err || resp.statusCode !== 200) console.log(err);
+    else characters = JSON.parse(body).characters;
+    const size = Object.keys(characters).length;
+    const array = Array(size).fill();
+    let data = 0;
+    for (let i = 0; i < size; i++) {
+	request(characters[i], (erro, respo, bodys) => {
+	    if (erro || respo.statusCode !== 200) console.log(erro);
+	    else {
+		array[i] = JSON.parse(bodys).name;
+		data++;
+	    }
+	    if (data === size) {
+		for (let j = 0; j < size; j++) {
+		    console.log(array[j]);
+		}
+	    }
+	}
+
+	       );
     }
-  }
 });
