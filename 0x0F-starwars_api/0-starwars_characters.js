6de2f559035968a8d211183097ca4581a78d2bb0
ledbagholberton <789@holbertonschool.com>
2020-06-03 21:04:43 +0000

    Include error condition related to statusCode

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,14 +5,14 @@ const args = process.argv.slice(2);
 const url = 'http://swapi-api.hbtn.io/api/films/' + args[0];
 
 request(url, async function (err, res, body) {
-  if (err) {
+  if (err || res.statusCode !== 200) {
     console.log(err);
   } else {
     const myList = JSON.parse(body).characters;
     for (const urlChar of myList) {
       const respChar = await new Promise((resolve, reject) => {
         request(urlChar, (error, response, html) => {
-          if (error) {
+          if (error || res.statusCode !== 200) {
             reject(error);
           } else {
             resolve(JSON.parse(html).name);
