865446b0f38729c533ec0870358207d25b7cc5c2
ledbagholberton <789@holbertonschool.com>
2020-06-03 19:53:01 +0000

    Change for Async

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,22 +3,23 @@
 const request = require('request');
 const args = process.argv.slice(2);
 const url = 'http://swapi-api.hbtn.io/api/films/' + args[0];
-const options = {
-    url: url,
-    method: 'GET',
-    headers: {
-	Accept: 'application/json',
-	'Accept-Charset': 'utf-8'
-    }
-};
 
-request(options, (err, res, body) => {
-    if (err) { throw err; }
+request(url, async function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
     const myList = JSON.parse(body).characters;
-    myList.forEach(function (element) {
-	request({ url: element, method: 'GET' }, (err, res, body) => {
-	    if (err) { console.log('Error'); }
-	    console.log(JSON.parse(body).name);
-	});
-    });
+    for (const urlChar of myList) {
+      const respChar = await new Promise((resolve, reject) => {
+        request(urlChar, (error, response, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(respChar);
+    }
+  }
 });
