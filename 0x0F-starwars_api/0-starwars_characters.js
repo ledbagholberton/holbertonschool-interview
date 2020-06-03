fa4ee55231220f10ee0a9f744df1e8c254aa340a
ledbagholberton <789@holbertonschool.com>
2020-06-03 23:47:21 +0000

    Semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,28 +2,27 @@
 
 const request = require('request');
 try {
-    const peli = process.argv.slice(2);
-    const url ='https://swapi-api.hbtn.io/api/films/' + peli[0];
-    request(url, async function (err, res, body) {
-	if (err || res.statusCode != 200) {
+  const peli = process.argv.slice(2);
+  const url = 'https://swapi-api.hbtn.io/api/films/' + peli[0];
+  request(url, async function (err, res, body) {
+    if (err || res.statusCode !== 200) {
 	    console.log(err);
-	} else {
+    } else {
 	    const myList = JSON.parse(body).characters;
 	    for (const urlChar of myList) {
-		const respChar = await new Promise((resolve, reject) => {
+        const respChar = await new Promise((resolve, reject) => {
 		    request(urlChar, (err_char, resp_char, body_char) => {
-			if (err_char || resp_char.statusCode != 200) {
+            if (err_char || resp_char.statusCode !== 200) {
 			    reject(error);
-			} else {
+            } else {
 			    resolve(JSON.parse(body_char).name);
-			}
+            }
 		    });
-		});
-		console.log(respChar);
+        });
+        console.log(respChar);
 	    }
-	}
-    });
+    }
+  });
 } catch (catchErr) {
-    console.log(catchErr);
+  console.log(catchErr);
 }
-
