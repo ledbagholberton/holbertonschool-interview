690e1dd19cebe9341bb799ced22670d9a363f6b2
ledbagholberton <789@holbertonschool.com>
2020-06-04 00:07:22 +0000

    Semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,28 +2,27 @@
 
 const request = require('request');
 try {
-    const peli = process.argv.slice(2);
-    const url ='https://swapi-api.hbtn.io/api/films/' + peli[0];
-    request(url, async function (err, res, body) {
-	if (err || res.statusCode != 200) {
-	    console.log(err);
-	} else {
-	    const myList = JSON.parse(body).characters;
-	    for (const urlChar of myList) {
-		const respChar = await new Promise((resolve, reject) => {
-		    request(urlChar, (err_char, resp_char, body_char) => {
-			if (err_char || resp_char.statusCode != 200) {
-			    reject(error);
-			} else {
-			    resolve(JSON.parse(body_char).name);
-			}
-		    });
-		});
-		console.log(respChar);
-	    }
-	}
-    });
+  const peli = process.argv.slice(2);
+  const url = 'https://swapi-api.hbtn.io/api/films/' + peli[0];
+  request(url, async function (err, res, body) {
+    if (err || res.statusCode !== 200) {
+      console.log(err);
+    } else {
+      const myList = JSON.parse(body).characters;
+      for (const urlChar of myList) {
+        const respChar = await new Promise((resolve, reject) => {
+          request(urlChar, (errChar, respChar, bodyChar) => {
+            if (errChar || respChar.statusCode !== 200) {
+              reject(errChar);
+            } else {
+              resolve(JSON.parse(bodyChar).name);
+            }
+          });
+        });
+        console.log(respChar);
+      }
+    }
+  });
 } catch (catchErr) {
-    console.log(catchErr);
+  console.log(catchErr);
 }
-
