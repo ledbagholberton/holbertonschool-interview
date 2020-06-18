224eea5af0125b0d0d3069212bb1970dfb28bc17
ledbagholberton <789@holbertonschool.com>
2020-06-18 01:53:52 +0000

    Betty

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * heap - 
+ * heap - heap the input
  *
  *@array: list of integers
  *@max_size: max size for each iteration
@@ -10,27 +10,27 @@
  */
 void heap(int *array, size_t max_size, size_t size, size_t index)
 {
-  size_t max, right, left;
-  int a;
+	size_t max, right, left;
+	int a;
 
-  max = index;
-  left = 2 * index + 1;
-  right = 2 + index * 2;
+	max = index;
+	left = 2 * index + 1;
+	right = 2 + index * 2;
 
-  if (left < max_size && array[left] > array[max])
-    max = left;
+	if (left < max_size && array[left] > array[max])
+		max = left;
 
-  if (right < max_size && array[right] > array[max])
-    max = right;
+	if (right < max_size && array[right] > array[max])
+		max = right;
 
-  if (max != index)
-    {
-      a = array[index];
-      array[index] = array[max];
-      array[max] = a;
-      print_array(array, size);
-      heap(array, max_size, size, max);
-    }
+	if (max != index)
+	{
+		a = array[index];
+		array[index] = array[max];
+		array[max] = a;
+		print_array(array, size);
+		heap(array, max_size, size, max);
+	}
 }
 
 /**
@@ -41,18 +41,18 @@ void heap(int *array, size_t max_size, size_t size, size_t index)
  */
 void heap_sort(int *array, size_t size)
 {
-  int b, prim;
+	int b, prim;
 
-  if(!array || size < 1)
-    return;
-  for(b = (size / 2) - 1; b >= 0; b--)
-    heap(array, size, size, b);
-  for(b = size - 1; b > 0; b--)
-    {
-      prim = array[0];
-      array[0] = array[b];
-      array[b] = prim;
-      print_array(array, size);
-      heap(array, b, size, 0);
-    }
+	if (!array || size < 1)
+	return;
+	for (b = (size / 2) - 1; b >= 0; b--)
+	heap(array, size, size, b);
+	for (b = size - 1; b > 0; b--)
+	{
+		prim = array[0];
+		array[0] = array[b];
+		array[b] = prim;
+		print_array(array, size);
+		heap(array, b, size, 0);
+	}
 }
