4397220478a964caec347c0048628def9b356b9d
ledbagholberton <789@holbertonschool.com>
2020-06-25 03:37:47 +0000

    w/o libraries not used

--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -1,9 +1,7 @@
 #ifndef _ALGOS_
 #define _ALGOS_
-#include <stdlib.h>
 #include <stdio.h>
 
-int binary_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 
 #endif
