bafefc06172f266e3e0620dde720d5dc711511e3
ledbagholberton <789@holbertonschool.com>
2020-07-09 04:09:19 +0000

    betty for .h

--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -14,17 +13,15 @@
  */
 struct binary_tree_s
 {
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
 void bt_preorder(heap_t *tree, heap_t **node, size_t h, size_t level);
 size_t bt_height(heap_t *tree);
 int heap_extract(heap_t **root);
 void binary_tree_print(const binary_tree_t *);
-
 #endif
