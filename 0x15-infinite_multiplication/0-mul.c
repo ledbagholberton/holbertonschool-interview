31b01c7ca2f6f4a326f5c1fcb257f0f9e5bff589
ledbagholberton <789@holbertonschool.com>
2020-07-16 04:31:37 +0000

    With betty

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -27,36 +27,33 @@ void error(void)
 
 int main(int argc, char *argv[])
 {
-	int i, j, tmp, l1, l2, l3, *a, *b, *ans;
-	char *s1, *s2;
+	int i, j, tmp, l1, l2, l3, *a, *b;
+	int *ans;
 
 	if (argc < 3 || argv[1] == NULL || argv[2] == NULL)
 		error();
 	l1 = strlen(argv[1]);
 	l2 = strlen(argv[2]);
-	l3 = l1 * l2;
-	s1 = malloc(l1);
-	s2 = malloc(l2);
-	s1 = argv[1];
-	s2 = argv[2];
+	l3 = l1 + l2;
+	ans = (int *)calloc(l3, sizeof(int));
 	a = malloc(l1);
 	b = malloc(l2);
-	ans = malloc(l1 + l2);
+
 	for (i = l1 - 1, j = 0; i >= 0; i--, j++)
 		{
-		a[j] = s1[i] - '0';
+		a[j] = argv[1][i] - '0';
 		if (a[j] < 0 || a[j] > 9)
 			error();
 		}
 	for (i = l2 - 1, j = 0; i >= 0; i--, j++)
 		{
-		b[j] = s2[i] - '0';
+		b[j] = argv[2][i] - '0';
 		if (b[j] < 0 || b[j] > 9)
 			error();
 		}
 	for (i = 0; i < l2; i++)
 		for (j = 0; j < l1; j++)
-			ans[i + j] += b[i] * a[j];
+			ans[i + j] = ans[i + j] + b[i] * a[j];
 	for (i = 0; i < l3; i++)
 		{
 		tmp = ans[i] / 10;
@@ -64,7 +61,8 @@ int main(int argc, char *argv[])
 		ans[i + 1] = ans[i + 1] + tmp;
 		}
 	for (i = l3 - 1; i >= 0; i--)
-		_putchar(ans[i] + '0');
+		if (i != l3 - 1 || ans[i] != 0)
+			_putchar(ans[i] + '0');
 	_putchar('\n');
 	return (0);
 }
