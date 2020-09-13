ce5edf0844a2716a56247abc49ce83b3ecdf281e
ledbagholberton <789@holbertonschool.com>
2020-09-12 21:52:51 -0500

    Radix Sort

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * radix_sort - implement radix sort
+ * @array: pointer to array to be sorted
+ * @size: size of array
+ * Return: No return
+ **/
+void radix_sort(int *array, size_t size)
+{
+	size_t i;
+	int bucket[10][10], b_cnt[10], j, k, r, NOP = 0, divisor = 1, lar, pass;
+
+	if (size < 2)
+		return;
+	lar = array[0];
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > lar)
+			lar = array[i];
+	}
+	while (lar > 0)
+	{
+		NOP++;
+		lar /= 10;
+	}
+	for (pass = 0; pass < NOP; pass++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			b_cnt[i] = 0;
+		}
+		for (i = 0; i < size; i++)
+		{
+			r = (array[i] / divisor) % 10;
+			bucket[r][b_cnt[r]] = array[i];
+			b_cnt[r] += 1;
+		}
+		i = 0;
+		for (k = 0; k < 10; k++)
+		{
+			for (j = 0; j < b_cnt[k]; j++)
+			{
+			array[i] = bucket[k][j];
+			i++;
+			}
+		}
+		divisor *= 10;
+		print_array(array, size);
+	}
+}
---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,8 @@
+#ifndef sort_h_
+#define sort_h_
+#include <stdio.h>
+
+void radix_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif
