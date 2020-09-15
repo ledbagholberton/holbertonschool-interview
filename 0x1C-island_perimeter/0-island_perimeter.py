f7d049b7181d018b701dcbae73788b8c8aa82b55
ledbagholberton <789@holbertonschool.com>
2020-09-15 03:07:54 -0500

    Include a border

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -6,10 +6,22 @@
 Function island parameter
 """
 
+def insert_bound(grid):
+    """
+    insert rows and cols
+    """
+    size_w = len(grid[0]) + 1
+    for i in grid:
+        i.insert(0, 0)
+        i.insert(size_w, 0)
+    g_i = [0] * len(grid[0])
+    grid.insert(0, g_i)
+    grid.insert(len(grid)+ 1, g_i)
 
 def island_perimeter(grid):
     """function perimeter
     """
+    insert_bound(grid)
     size_h = len(grid)
     size_w = len(grid[0])
     b = 0
