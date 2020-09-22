caa4aaa5b449377ee4f037571c1dd1a655cbac44
ledbagholberton <789@holbertonschool.com>
2020-09-22 18:36:13 -0500

    Change the type def

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -17,7 +17,7 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-
+typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
